638235fd2a080afa223020387927635575896212
Zemtzov7 <72821250+zmv7@users.noreply.github.com>
2023-01-10 19:53:10 +0500

    Fix trash in craft page & improvements

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
-local is_MC = false
+local W = 8
 local lists = {}
 local inv = {}
 local tab = 1
@@ -7,10 +7,13 @@ local page = 1
 local function inv_fs()
 	local list = lists[tab] or "craft"
 	local size = inv[list] and #inv[list] or 32
-	local fs = "size["..(is_MC and "9.4" or "8.4")..",9.5]" ..
+	local fs = "size["..(W+0.4)..",9.5]" ..
 		"listcolors[#777;#AAA]" ..
 		"tabheader[0,0;tabs;"..table.concat(lists,",")..";"..(tab or "1")..";false;false]" ..
-		"list[current_player;main;0.2,5.5;"..(is_MC and "9" or "8")..",4;0]" ..
+		"label[0.3,-0.3;"..(lists[tab] or "").."]" ..
+		"label["..(W-1.5)..",4.6;Trash:]" ..
+		"list[detached:trash;main;"..(W-0.8)..",4.4;1,1;0]" ..
+		"list[current_player;main;0.2,5.5;"..W..",4;0]" ..
 		(list == "craft" and
 		"list[current_player;craft;"..(inv["craft"] and #inv["craft"] == 4 and "3,0;2,2" or "2,0;3,3")..";0]" ..
 		"label[5.3,0.2;->]" ..
@@ -19,9 +22,8 @@ local function inv_fs()
 		"listring[current_player;main]" ..
 		"listring[current_player;craft]"
 		or
-		"list[current_player;"..list..";0.2,0.2;"..(is_MC and "9" or "8")..",4;"..(size > 31 and tostring(32*page-32) or "0").."]" ..
-		"label["..(is_MC and "7.5" or "6.5")..",4.6;Trash:]" ..
-		"list[detached:trash;main;"..(is_MC and "8.2" or "7.2")..",4.4;1,1;0]" ..
+		"list[current_player;"..list..";0.2,0.2;"..W..",4;"..(size >= (W*4) and tostring((W*4)*page-(W*4)) or "0").."]" ..
+		"listring[]" ..
 		(size > 32 and
 		"button[0.2,4.4;1,1;prevpage;<-]" ..
 		"field[1.3,4.7;1.4,1;page;;"..(page or "1").."]" ..
@@ -41,7 +43,7 @@ core.register_on_inventory_open(function(inventory)
 	table.sort(lists)
 	table.insert(lists,1,"craft")
 	if inv["main"] and #inv["main"] == 36 then
-		is_MC = true
+		W = 9
 	end
 	local ctrl = core.localplayer:get_control()
 	if ctrl and ctrl.aux1 and not ctrl.sneak then
@@ -55,6 +57,7 @@ core.register_chatcommand("cinv",{
   description = "Open Custom Inventory",
   func = function(param)
 	inv_fs()
+	return true, table.concat(lists,", ")
 end})
 core.register_on_formspec_input(function(formname,fields)
 	if formname ~= "cinv" then return end
